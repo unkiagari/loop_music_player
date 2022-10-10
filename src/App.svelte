8376690af1f711d94623381032e6cdf634fa27ae
agari <unkiagari@outlook.jp>
2022-10-10 23:25:25 +0900

    本体完成

--- src/App.svelte
+++ src/App.svelte
@@ -3,17 +3,34 @@
   import * as fs from "@tauri-apps/api/fs";
   import { homeDir } from "@tauri-apps/api/path";
   import { convertFileSrc } from "@tauri-apps/api/tauri";
+  import { appWindow, LogicalSize } from "@tauri-apps/api/window";
+  import { onDestroy } from "svelte";
+  import LoopAudio from "./LoopAudio";
   // console.log(fs.readDir())
-
+  const loopAudio = new LoopAudio();
   const settings = {
     audioDirPath: "",
     duration: 10,
+    crossfade: 3,
+    volume: 0.5,
     ...JSON.parse(localStorage.getItem("settings") || "{}"),
-  } as { audioDirPath: string; duration: number };
+  } as {
+    audioDirPath: string;
+    duration: number;
+    crossfade: number;
+    volume: number;
+  };
 
-  let files = [];
+  let isPlaying = false;
+  loopAudio.onChangePlayState = (_isPlaying) => {
+    isPlaying = _isPlaying;
+  };
+
   const selectAudioDir = async () => {
-    const dir = (await d.open({ directory: true })) as string | null;
+    const dir = (await d.open({
+      directory: true,
+      defaultPath: settings.audioDirPath,
+    })) as string | null;
     if (!dir) return;
     settings.audioDirPath = dir;
     save();
@@ -24,34 +41,64 @@
     if (isNaN(settings.duration) || settings.duration < 1) {
       settings.duration = 1;
     }
+    if (isNaN(settings.crossfade) || settings.crossfade < 1) {
+      settings.crossfade = 1;
+    }
+    if (isNaN(settings.volume)) {
+      settings.volume = 0.5;
+    }
+    settings.volume = Math.min(1, Math.max(0, settings.volume));
+
     localStorage.setItem("settings", JSON.stringify(settings));
   };
   const load = async () => {
     if (!settings.audioDirPath) return;
-    files = await fs.readDir(settings.audioDirPath);
+    loopAudio.files = (await fs.readDir(settings.audioDirPath))
+      .filter((v) => v.name.endsWith(".mp3"))
+      .map((v) => ({
+        name: v.name,
+        path: convertFileSrc(v.path).replace(/%2F/g, "/"),
+      }));
+    loopAudio.crossfade = 3;
+    loopAudio.audioDuration = settings.duration;
   };
   load();
 
-  let audioElmIndex = 0;
-  const play = async () => {
-    const files = fs.readDir(settings.audioDirPath);
-  };
+  onDestroy(() => loopAudio.stop());
 
-  const audios = Array(2).fill(document.createElement("audio"));
+  let isShowSettings = false;
+  $: {
+    const barSize = 25;
+    const size = isShowSettings
+      ? new LogicalSize(250, 200 + barSize)
+      : new LogicalSize(250, 40 + barSize);
+    appWindow.setSize(size);
+  }
+
+  const getDirName = (path: string) => {
+    const f = path.split("/");
+    return f[f.length - 1];
+  };
 </script>
 
-{#if !settings.audioDirPath}
-  <button on:click={selectAudioDir}>select audio dir</button>
-{:else}
-  <button>play</button>
-  <button>stop</button>
+{#if settings.audioDirPath}
+  <button class:active={isPlaying} on:click={() => loopAudio.play()}
+    >play</button>
+  <button on:click={() => loopAudio.stop()}>stop</button>
+  <button
+    class:active={isShowSettings}
+    on:click={() => (isShowSettings = !isShowSettings)}>settings</button>
+{/if}
+
+{#if isShowSettings || !settings.audioDirPath}
   <hr />
   <div>
     <table>
       <tr>
         <td>audioDir</td>
         <td>
-          <button on:click={selectAudioDir}>{settings.audioDirPath}</button>
+          <button on:click={selectAudioDir}
+            >{getDirName(settings.audioDirPath)}</button>
         </td>
       </tr>
       <tr>
@@ -60,11 +107,41 @@
           <input
             type="number"
             min="1"
-            pattern="0-9"
             bind:value={settings.duration}
             on:change={save} />
+        </td>
+      </tr>
+      <tr>
+        <td>crossfade</td>
+        <td>
+          <input
+            type="range"
+            min="1"
+            max="5"
+            bind:value={settings.crossfade}
+            on:change={save} />
+          {settings.crossfade}
+        </td>
+      </tr>
+      <tr>
+        <td>volume</td>
+        <td>
+          <input
+            type="range"
+            min="0"
+            max="1"
+            step=".05"
+            bind:value={settings.volume}
+            on:change={save} />
+          {settings.volume}
         </td>
       </tr>
     </table>
   </div>
 {/if}
+
+<style>
+  td > input {
+    vertical-align: middle;
+  }
+</style>
---
+++ src/LoopAudio.ts
@@ -0,0 +1,81 @@
+interface IFile {
+  name: string
+  path: string
+}
+export default class LoopAudio {
+  volume = .1
+  crossfade = 3
+  audioDuration = 10
+
+  audioElmIndex = 0
+  audioFileIndex = 0
+
+  audios = Array(2).
+    fill(0).map(v => {
+      const audio = document.createElement("audio")
+      audio.loop = true
+      return audio
+    })
+
+  files = [] as IFile[]
+  shuffledFiles = [] as IFile[]
+
+  intervalId = 0 as any
+  timeoutId = 0 as any
+
+  onChangePlayState = (isPlaying: boolean) => { }
+  updatePlayingState() {
+    const isPlaying = !this.audios.every(v => v.paused)
+    this.onChangePlayState(isPlaying)
+  }
+
+  play = () => {
+    if (this.audios.every(v => v.paused)) {
+      this.audioFileIndex = 0
+      this.audioElmIndex = 0
+      this.shuffledFiles = shuffle(this.files)
+      this.audios.forEach(v => v.volume = 0)
+    }
+
+    const prevAudio = this.audios[(this.audioElmIndex) % 2]
+    const newAudio = this.audios[(this.audioElmIndex + 1) % 2]
+    newAudio.src = this.shuffledFiles[this.audioFileIndex % this.shuffledFiles.length].path
+    newAudio.volume = 0
+    newAudio.play()
+    this.audioFileIndex++
+    this.audioElmIndex++
+    this.updatePlayingState()
+
+    const step = this.volume / (1000 / 60) / this.crossfade
+
+    clearInterval(this.intervalId)
+    this.intervalId = 0
+    this.intervalId = setInterval(() => {
+      prevAudio.volume = Math.max(0, prevAudio.volume - step)
+      newAudio.volume = Math.max(this.volume, prevAudio.volume + step)
+      if (prevAudio.volume === 0) prevAudio.pause()
+      if (prevAudio.volume === 0
+        && newAudio.volume === this.volume) {
+        clearInterval(this.intervalId)
+        this.intervalId = 0
+      }
+    }, 1000 / 60)
+
+    clearTimeout(this.timeoutId)
+    this.timeoutId = setTimeout(this.play, this.audioDuration * 1000 * 60)
+  }
+  stop() {
+    clearInterval(this.intervalId)
+    clearTimeout(this.timeoutId)
+    this.audios.forEach(v => v.pause())
+    this.updatePlayingState()
+  }
+}
+
+const shuffle = ([...array]) => {
+  for (let i = array.length - 1; i >= 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
